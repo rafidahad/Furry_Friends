289adbbda187f129d7e183ce81da286decbc807d
rafidahad <rafidahad123@gmail.com>
2025-03-02 22:42:42 +0600

    USER MODELUPDATED

--- FurryFriendsBackend/model/user.js
+++ FurryFriendsBackend/model/user.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
+  username: { type: String, required: true, unique: true }, // New username field
   emailOrPhone: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
   verified: { type: Boolean, default: false },
